a42295dec536a9a6e1ad358f2a5e55d9536ca328
rindev6 <52327516+rindev6@users.noreply.github.com>
2024-08-04 16:17:48 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
+const browser = typeof window !== 'undefined' ? window.browser || window.chrome : self.browser || self.chrome;
 browser.runtime.onInstalled.addListener(() => {
     browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-      if (tab.url.includes('instagram.com')) {
-        browser.tabs.executeScript(tabId, { file: 'content.js' });
-      }
+        if (tab.url && tab.url.includes('instagram.com')) {
+            browser.scripting.executeScript({
+                target: { tabId: tabId },
+                files: ['content.js']
+            });
+        }
     });
-  });
+});
